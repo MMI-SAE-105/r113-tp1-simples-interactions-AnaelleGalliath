e7c5172ed1f008e94ad72dac5fafd715723ac6f2
AnaelleGalliath <anaelle.galliath@edu.univ-fcomte.fr>
2024-11-27 15:20:59 +0100

    h2

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,14 @@ enRougeSuiteAClick.addeventListener("click", (evt)=>{
     enRougeSuiteAClick.style.color = "red"
     });
 
-    
+
+collH2 = document.querySelector("h2")
+
+collH2.forEach ((elm)=>{
+    elm.addeventListener("click".at(evt)=>{
+        evt.target.style.color = "red";
+    })
+})
+
+
+// J'ai tout réecris ici car je n'étais pas sur que ca marche dans la console
