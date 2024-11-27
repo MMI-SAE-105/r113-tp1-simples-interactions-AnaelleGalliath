838f4958e16f98df9dcc977a1a4ab8393c2a2141
AnaelleGalliath <anaelle.galliath@edu.univ-fcomte.fr>
2024-11-27 15:17:02 +0100

    en Rouge suite a un click

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,10 @@ aMettreEnRouge = document.querySelectorAll("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red";
 
 
+enRougeSuiteAClick = document.getElementById("en-rouge-suite-a-click")
+
+enRougeSuiteAClick.addeventListener("click", (evt)=>{
+    enRougeSuiteAClick.style.color = "red"
+    });
+
+    
